db3302549630fac29f462f5a78a82ae3f094995e
Alyssa Dang <alyssadang@tamu.edu>
2019-10-12 21:35:25 -0500

    Create Connect.java
    This is a program that connects to the database: jdbc:postgresql://db-315.cse.tamu.edu/hnpatel97_db
    After connecting to the database, it opens a JFrame window and asks the user what to search for from the database.

---
+++ Connect.java
@@ -0,0 +1,145 @@
+import java.sql.*;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.*;
+public class Connect
+{
+   // JDBC driver name and database URL
+   static final String JDBC_DRIVER = "org.postgresql.Driver";  
+   static final String DB_URL = "jdbc:postgresql://db-315.cse.tamu.edu/hnpatel97_db";
+   static final String USER = "hnpatel97";
+   static final String PASS = "studentpwd";
+   
+   public static void main(String[] args) 
+   {
+	   Connection conn = null;
+	   Statement stmt = null;
+	   String sql = "";
+	   try
+	   {
+		   //Open a connection
+		   Class.forName("org.postgresql.Driver");
+		   System.out.println("Connecting to database...");
+		   conn = DriverManager.getConnection(DB_URL,USER,PASS);
+		   
+		   //Creates the Window
+		   mainFrame mainWindow = new mainFrame();
+
+		   String search = mainWindow.getSearch();
+		   String category = mainWindow.getCategory();
+		   
+		   //stmt = conn.createStatement();
+
+		   conn.close();
+		
+	   }
+	   catch(SQLException se)
+	   {
+		  //Handle errors for JDBC
+		  se.printStackTrace();
+	   }catch(Exception e){
+		  //Handle errors for Class.forName
+		  e.printStackTrace();
+	   }
+	   finally //finally block used to close resources
+	   {
+		  try
+		  {
+			 if(stmt!=null)
+				stmt.close();
+		  }
+		  catch(SQLException se2)
+		  {
+			  // nothing to do
+		  }
+		  try
+		  {
+			 if(conn!=null)
+			 {
+				conn.close();
+			 }
+		  }catch(SQLException se)
+		  {
+			 se.printStackTrace();
+		  }
+	   }//end try
+	   System.out.println(" Goodbye!");
+	}
+}
+
+class mainFrame implements ActionListener
+{
+	private JFrame frame;
+	private String searchFromBar = "";
+	private String searchCategory = "";
+    //public boolean frameRunning = true;
+    //public boolean searchRunning = true;
+    mainFrame() 
+    {   
+        // creating instance of JFrame with name "Main Window" 
+        frame=new JFrame("Main Window");
+        
+        //creates instance of search bar
+        JTextField searchBar = new JTextField();
+        searchBar.setBounds(300, 50, 200, 20); 
+        searchBar.addActionListener(this);
+        System.out.println(searchBar.getText());
+        frame.add(searchBar);
+        
+        //creates instance of combo box
+        String category[] = {"Movies", "TV Shows", "People","Ratings"};
+        JComboBox sCategory = new JComboBox(category);
+        sCategory.setBounds(200,50, 100, 20);
+        frame.add(sCategory);
+        
+        // creates instance of JButton 
+        JButton sButton = new JButton("Search"); 
+        sButton.setBounds(500, 50, 100, 20);
+        sButton.addActionListener(new ActionListener() 
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) 
+            {
+            	String s = e.getActionCommand(); 
+                if (s.equals("Search")) 
+                {
+                	searchFromBar = searchBar.getText();
+                	searchCategory = (String)sCategory.getItemAt(sCategory.getSelectedIndex());
+                	System.out.println(searchFromBar); // PRINTS THE STRING FROM SEARCH BAR
+                	System.out.println(searchCategory); // PRINTS THE CATEGORY
+                }
+            }
+        });
+        frame.add(sButton);
+        
+        //creates instance of text
+        JLabel label = new JLabel("Search: ");
+        label.setBounds(150, 10, 90, 100);
+        frame.add(label);
+
+        // sets 800 width and 600 height 
+        frame.setSize(800, 600);
+          
+        // uses no layout managers 
+        frame.setLayout(null); 
+        
+        // This is the closing condition
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // makes the frame visible 
+        frame.setVisible(true); 
+    }
+    public void actionPerformed(ActionEvent e) 
+    { 
+    	//Nothing here yet
+    } 
+    public String getSearch() //RETURNS STRING FROM SEARCH BAR
+    {
+        return searchFromBar;
+    }
+    public String getCategory() //RETURNS THE CATEGORY
+    {
+        return searchCategory;
+    }
+}
